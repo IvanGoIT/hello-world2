42e8e3028c4acd6dcbd10102e5fc5666e29a7a0e
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2017-06-23 20:51:23 +0300

    добавили текст

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
-/**
- * Created by baizor on 23.06.17.
- */
+
 public class Main {
     public static void main(String[] args) {
-        // code
+        System.out.println("hello world");
+        System.out.println("еще одна строка текста");
     }
 }
